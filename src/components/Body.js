3704e88b239ba509e984c79f829d43b2ed1f44a4
chirag4851 <chiragchavda1243@gmail.com>
2022-12-22 17:02:08 +0530

    initial

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+function Body (props){
+    return <div>
+    {
+      props.image.map(image=>{
+        return(
+          <div key={image.id} className="column">
+            <img src={image.img} alt=""></img>
+          </div>
+        )
+      })
+    }
+  </div>
+}
+
+export default Body
